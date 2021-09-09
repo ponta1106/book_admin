ee486fd7f9a869d2683974c288092654e4939a9c
ponta1106 <qqkz2qf9k@kind.ocn.ne.jp>
2021-09-09 20:37:36 +0900

    Add: ユーザーモデルを追加。

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,21 @@
+class ProfilesController < ApplicationController
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    user = current_user
+    user.update(user_params)
+  end
+
+  private
+  def user_params
+    if current_user.admin?
+      params.require(:user).permit(:name, :email, :admin)
+    else
+      params.require(:user).permit(:name, :email)
+    end
+  end
+end
